89f53c8577202c63aa67aecc736659a2f967b1b6
Margaret K. Geiger <mkg@fastmail.com>
2019-05-10 16:37:36 -0700

    initial commit

---
+++ lib/encrust.rb
@@ -0,0 +1,6 @@
+require "encrust/version"
+
+module Encrust
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/encrust_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Encrust do
+  it "has a version number" do
+    expect(Encrust::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
