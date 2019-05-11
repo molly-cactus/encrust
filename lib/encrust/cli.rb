bf01485ef41e675d508824b960411ae902791428
Margaret K. Geiger <mkg@fastmail.com>
2019-05-11 02:37:57 -0700

    handle invalid input

--- lib/encrust/cli.rb
+++ lib/encrust/cli.rb
@@ -3,10 +3,81 @@ require 'encrust'
 module Encrust
   class CLI
     class << self
+      def start
+        paths = ARGV.map(&:freeze)
+        puts greeting(paths)
+        errors = convert(paths)
+        puts error_message(errors) if errors.any?
+        puts instructions if paths.size > errors.size
+        puts exit_message
+      end
+
       private
 
-      def underline(text)
-        "\e[4m#{text}\e[0m"
+      def convert(files)
+        puts
+        errors = []
+        files.each do |path|
+          print "  ☼ #{File.basename(path)} -> "
+          begin
+            colors = Converter.iterm_to_df(path)
+          rescue ArgumentError => e
+            STDERR.puts "ERROR \e[31;1m✕\e[0m"
+            errors << path
+          else
+            file = "#{File.basename(path, ".*")}.colors.txt"
+            File.write(file, colors)
+            puts "«#{file}» \e[32;1m✓\e[0m"
+          end
+        end
+        errors
+      end
+
+      def greeting(paths)
+        if paths.count == 1
+          "Converting an iterm2 color scheme to a Dwarf Fortress color file."
+        else
+          "Converting #{paths.count} iTerm2 color schemes " \
+            "to Dwarf Fortress color files."
+        end
+      end
+
+      def instructions
+        <<~DWARF
+
+          To use your colors in Dwarf Fortress, move the file to:
+
+            <your_dwarf_fortress_path>/data/init/colors.txt
+
+          Consider backing up the existing colors.txt file first.
+        DWARF
+      end
+
+      def error_message(paths_with_errors)
+        plural = paths_with_errors.size > 1 ? 's' : nil
+        <<~DWARF
+
+          Error#{plural} while parsing file#{plural} at path#{plural}:
+
+          #{paths_with_errors.map { |p| "  #{p}" }.join("\n")}
+
+          Please make sure to use valid, suitable iTerm2 color files:
+            ☼ They should be in PLIST (XML) format and contain <dict> tags.
+            ☼ They should specify RGB values for all ANSI colors from 0 to 15,
+              with each color's key formatted like e.g. `Ansi 0 Color` for 0.
+            ☼ Each RGB value should lie between 0 and 1, both inclusive.
+        DWARF
+      end
+
+      def exit_message
+        <<~DWARF
+
+          To support the development of Dwarf Fortress, go to:
+            ☼ Bay 12 Games's patreon: #{patreon}
+            ☼ Bay 12 Games's website: #{bay_12_site}
+
+          Thanks for using encrust, and remember: Losing is fun!
+        DWARF
       end
 
       def patreon
@@ -17,37 +88,8 @@ module Encrust
         underline("http://bay12games.com/support.html")
       end
 
-      def message
-        <<~DWARF
-
-          To use your colors in Dwarf Fortress, move the file to:
-
-            <your_dwarf_fortress_path>/data/init/colors.txt
-
-          Consider backing up the existing colors.txt file first.
-
-          To support the development of Dwarf Fortress, go to:
-            * Bay 12 Games's patreon: #{patreon}
-            * Bay 12 Games's website: #{bay_12_site}
-
-          Thanks for using encrust, and remember: Losing is fun!
-        DWARF
-      end
-
-      def convert(files)
-        files.each do |path|
-          colors = Encrust.with_gems(path)
-          file = "#{File.basename(path, ".*")}.colors.txt"
-          File.write(file, colors)
-          puts "💎 #{File.basename(path)} -> #{file}"
-        end
-      end
-
-      public
-
-      def start
-        convert(ARGV)
-        puts message
+      def underline(text)
+        "\e[4m#{text}\e[0m"
       end
     end
   end
--- lib/encrust/converter.rb
+++ lib/encrust/converter.rb
@@ -1,52 +1,78 @@
 require 'plist'
 
 module Encrust
-  class << self
-    private
+  class Converter
+    class << self
+      def iterm_to_df(path)
+        header << colors(path) << footer(path)
+      end
 
-    # ANSI Color 3 ("yellow") is called "BROWN" in Dwarf Fortress,
-    # while ANSI Color 11 ("bright yellow") is simply called "YELLOW".
-    # ANSI Color 7 ("white") is called "LGRAY" in Dwarf Fortress,
-    # ANSI Color 8 ("bright black") is called "DGRAY", and
-    # ANSI Color 15 ("bright white") is called "WHITE".
-    def names
-      %i[BLACK  RED  GREEN  BROWN  BLUE  MAGENTA  CYAN LGRAY
-         DGRAY LRED LGREEN YELLOW LBLUE LMAGENTA LCYAN WHITE]
-    end
+      private
 
-    def colors(path)
-      iterm_colors = Plist.parse_xml(path)
-      names.map.with_index do |color, i|
-        iterm_rgb = iterm_colors["Ansi #{i} Color"]
+      # ANSI Color 3 ("yellow") is called "BROWN" in Dwarf Fortress,
+      # while ANSI Color 11 ("bright yellow") is simply called "YELLOW".
+      # ANSI Color 7 ("white") is called "LGRAY" in Dwarf Fortress,
+      # ANSI Color 8 ("bright black") is called "DGRAY", and
+      # ANSI Color 15 ("bright white") is called "WHITE".
+      def df_color_names
+        %i[BLACK  RED  GREEN  BROWN  BLUE  MAGENTA  CYAN LGRAY
+           DGRAY LRED LGREEN YELLOW LBLUE LMAGENTA LCYAN WHITE]
+      end
+
+      def parse(path)
+        begin
+          Plist.parse_xml(path)
+        rescue RuntimeError => e
+          msg = "Failed to parse file at path: #{path}"
+          raise ArgumentError.new(msg)
+        end
+      end
+
+      def colors(path)
+        iterm_colors = parse(path)
+        df_color_names.each_with_index.reduce("") do |df_colors, (color, i)|
+          iterm_rgb = iterm_colors["Ansi #{i} Color"]
+          if iterm_rgb.nil?
+            msg = "Missing color definition for #{color} (DF) / #{i} (ANSI)"
+            raise ArgumentError.new(msg)
+          end
+          df_colors << generate_df_color(color, iterm_rgb)
+        end
+      end
+
+      def generate_df_color(color, iterm_rgb)
+        red   = (iterm_rgb['Red Component'] * 255).to_i
+        green = (iterm_rgb['Green Component'] * 255).to_i
+        blue  = (iterm_rgb['Blue Component'] * 255).to_i
+        
+        if [red, green, blue].any? { |x| x > 255 || 0 > x }
+          msg = "RGB value for #{color} (DF) out of valid range."
+          raise ArgumentError.new(msg)
+        end
+
         <<~DWARF
-          [#{color}_R:#{(iterm_rgb['Red Component'] * 255).to_i}]
-          [#{color}_G:#{(iterm_rgb['Green Component'] * 255).to_i}]
-          [#{color}_B:#{(iterm_rgb['Blue Component'] * 255).to_i}]
+            [#{color}_R:#{red}]
+            [#{color}_G:#{green}]
+            [#{color}_B:#{blue}]
         DWARF
-      end.join
-    end
+      end
 
-    def header
-      <<~DWARF
-        These are the display colors in RGB.  \
-        The game is actually displaying extended \
-        ASCII characters in OpenGL, so you can modify the colors.
+      def header
+        <<~DWARF
+          These are the display colors in RGB.  \
+          The game is actually displaying extended \
+          ASCII characters in OpenGL, so you can modify the colors.
 
-      DWARF
-    end
+        DWARF
+      end
 
-    def footer(path)
-      <<~DWARF
+      def footer(path)
+        <<~DWARF
 
-        Converted from `#{File.basename(path)}` with encrust:
-        https://github.com/smoochbot/encrust
-      DWARF
-    end
-
-    public
-
-    def with_gems(path)
-      header << colors(path) << footer(path)
+          Converted from `#{File.basename(path)}` using encrust:
+          https://github.com/smoochbot/encrust
+        DWARF
+      end
     end
   end
 end
